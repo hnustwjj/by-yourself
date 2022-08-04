6592ff1084b05088120c9b94365e08db08d88891
hnustwjj <948065841@qq.com>
2022-08-04 10:53:15 +0800

    update

--- ts/medium/6、##Chainable Options .ts
+++ ts/medium/6、##Chainable Options .ts
@@ -5,7 +5,7 @@ const result = config
   .option("name", "type-challenges")
   .option("bar", { value: "Hello World" })
   .get()
-
+// https://github.com/type-challenges/type-challenges/issues/13951
 type Chainable<T = {}> = {
   option: <K extends string, V>(
     key: K extends keyof T ? (V extends T[K] ? never : K) : K,  //判断传入的K是不是在T中已存在，如果存在再继续判断V与原先的是否一样
