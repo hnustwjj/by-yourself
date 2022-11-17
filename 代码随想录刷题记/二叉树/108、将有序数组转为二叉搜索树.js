cf95e971880b7c62371ef462d957873b6a00d365
wjj <948065841@qq.com>
2022-11-17 18:31:10 +0800

    update

---
+++ 代码随想录刷题记/二叉树/108、将有序数组转为二叉搜索树.js
@@ -0,0 +1,12 @@
+// 其实就是利用递归的语意，根据nums构建二叉搜索树
+// 为了让二叉搜索树尽量平衡，我们需要截取中间的元素
+var sortedArrayToBST = function(nums) {
+  if(nums.length === 0) return null
+  const mid = ~~(nums.length / 2)
+  const node = new TreeNode(nums[mid])
+  const left = sortedArrayToBST(nums.slice(0,mid))
+  const right = sortedArrayToBST(nums.slice(mid+1))
+  node.left = left
+  node.right = right
+  return node
+};
