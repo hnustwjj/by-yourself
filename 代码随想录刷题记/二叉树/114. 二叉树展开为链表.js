a3c499681147172189441024eff2aa34691fe0db
wjj <948065841@qq.com>
2022-11-22 18:47:25 +0800

    update

---
+++ 代码随想录刷题记/二叉树/114. 二叉树展开为链表.js
@@ -0,0 +1,24 @@
+// 利用递归的语意，对左右子树进行构建操作，然后讲左右子树构建出来的链表拼接到right指针
+// 不要忘记讲left指向空
+var flatten = function (r) {
+  function dfs(root) {
+    if (!root) return;
+    let node = root;
+
+    const left = dfs(root.left);
+    const right = dfs(root.right);
+
+    node.left = null;
+    if (left) {
+      node.right = left;
+      while (node.right) {
+        node = node.right;
+      }
+    }
+    if (right) {
+      node.right = right;
+    }
+    return root;
+  }
+  dfs(r);
+};
