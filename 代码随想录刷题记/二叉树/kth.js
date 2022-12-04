c43d5b3677f4f4f806b3740326186be8b70358f1
hnustwjj <948065841@qq.com>
2022-12-04 13:17:21 +0800

    update

--- 代码随想录刷题记/二叉树/kth.js
+++ 代码随想录刷题记/二叉树/kth.js
@@ -52,3 +52,112 @@ var findKthLargest = function (nums, k) {
   }
 };
 
+
+
+
+//  第k大数，就是维护k个长度的优先队列，如果长度超过k，就将队首弹出进行heapify操作
+ var KthLargest = function(k, nums) {
+  this.k = k
+  this.minHeap = new MinHeap(nums)
+};
+
+KthLargest.prototype.add = function(val) {
+  this.minHeap.offer(val)
+  while(this.minHeap.size() > this.k){
+      this.minHeap.poll()
+  }
+  return this.minHeap.peek()
+};
+
+class MinHeap {
+constructor(data = []) {
+  this.data = data;
+  this.comparator = (a, b) => a - b;
+  this.heapify();
+}
+
+heapify() {
+  if (this.size() < 2) return;
+  for (let i = 1; i < this.size(); i++) {
+    this.bubbleUp(i);
+  }
+}
+
+// 队顶
+peek() {
+  if (this.size() === 0) return null;
+  return this.data[0];
+}
+
+// 入队
+offer(value) {
+  this.data.push(value);
+  this.bubbleUp(this.size() - 1);
+}
+
+// 出队
+poll() {
+  if (this.size() === 0) {
+    return null;
+  }
+  const result = this.data[0];
+  const last = this.data.pop();
+  if (this.size() !== 0) {
+    this.data[0] = last;
+    this.bubbleDown(0);
+  }
+  return result;
+}
+
+// 向上执行heapInsert操作
+bubbleUp(index) {
+  while (index > 0) {
+    const parentIndex = (index - 1) >> 1;
+    if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    } else {
+      break;
+    }
+  }
+}
+
+// 向下执行heapify操作
+bubbleDown(index) {
+  const lastIndex = this.size() - 1;
+  while (true) {
+    const leftIndex = index * 2 + 1;
+    const rightIndex = index * 2 + 2;
+    let findIndex = index;
+    if (
+      leftIndex <= lastIndex &&
+      this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
+    ) {
+      findIndex = leftIndex;
+    }
+    if (
+      rightIndex <= lastIndex &&
+      this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
+    ) {
+      findIndex = rightIndex;
+    }
+    if (index !== findIndex) {
+      this.swap(index, findIndex);
+      index = findIndex;
+    } else {
+      break;
+    }
+  }
+}
+
+swap(index1, index2) {
+  [this.data[index1], this.data[index2]] = [
+    this.data[index2],
+    this.data[index1],
+  ];
+}
+
+size() {
+  return this.data.length;
+}
+}
